3c4ecc6044f67ec295f5c96728754693ec7957be
yutak <soano2428@gmail.com>
2023-09-14 13:16:29 +0900

    23.09.14 hsv 반영

--- 15_HSV(Hue 활용).py
+++ 15_HSV(Hue 활용).py
@@ -1,15 +1,6 @@
-'''
-색상(Hue) - 빨강, 노랑, 파랑 등으로 인식되는 색상 중 하나 또는 둘 이상의 색조합
-과 같이 시각적 감각의 속성
-
-채도(Saturation) - 이미지의 색상 깊이, 색상이 얼마나 순수한 색인지 의미
-
-명도(Value) - 색의 밝고 어두운 정도를 의미
-'''
-
 import cv2
 
-src = cv2.imread("Image/tomato.jpg", cv2.IMREAD_COLOR)
+src = cv2.imread("tomato.jpg", cv2.IMREAD_COLOR)
 hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 h, s, v = cv2.split(hsv)
 
